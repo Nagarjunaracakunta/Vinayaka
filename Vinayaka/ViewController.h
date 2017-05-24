fb15177cff2caaccf52d1bb1feace0e2de1f3800
NAGARJUNA <nagarjuna@NAGARJUNAs-MacBook-Air.local>
2017-05-24 11:29:22 +0530

    Initial Commit

---
+++ Vinayaka/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Vinayaka
+//
+//  Created by NAGARJUNA on 24/05/17.
+//  Copyright © 2017 NAGARJUNA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Vinayaka/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Vinayaka
+//
+//  Created by NAGARJUNA on 24/05/17.
+//  Copyright © 2017 NAGARJUNA. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
